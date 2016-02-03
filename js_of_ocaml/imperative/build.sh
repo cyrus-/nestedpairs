b1a8ab3d69cddc4a438f52045f69707ed41f0ddc
Cyrus Omar <cyology@gmail.com>
2016-02-03 03:08:59 -0500

    camlp4 in build file

--- js_of_ocaml/imperative/build.sh
+++ js_of_ocaml/imperative/build.sh
@@ -5,6 +5,7 @@ ocamlbuild -use-ocamlfind \
   -pkg tyxml \
   -pkg js_of_ocaml.tyxml \
   -pkg js_of_ocaml.syntax \
+  -syntax camlp4o \
   index.byte ;
 
 js_of_ocaml +weak.js -o index.ml.js index.byte
